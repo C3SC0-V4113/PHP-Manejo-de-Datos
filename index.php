b34a789c62d3abb52e7c845554b280998595907f
Francisco Valle <vallecesco@gmail.com>
2021-01-06 19:04:11 -0600

    Creando un documento php nativo

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+/*Comentario
+de
+Varias lineas */
+
+class Answer{
+    /**
+     * Comentario de varias
+     * lineas para
+     * palabra
+     */
+
+    protected $clients=[];  //Mi comentario
+    protected $insurers=[];
+}
