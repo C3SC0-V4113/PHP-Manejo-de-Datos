e9579b79eeb93bfde5aaf40f0db45af054a4aa06
Francisco Valle <vallecesco@gmail.com>
2021-01-07 15:59:58 -0600

    Probando una funcion echo usando el composer

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-//Asignación
-$num=9;
-$lang=['es'=>'español','en'=>'inglés'];
+require __DIR__ . '/vendor/autoload.php';
 
-//Aritmética
-echo "Suma 2+2 " . ((int)2+(int)2);
-echo "Resta 2-2 " . ((int)2-(int)2);
-echo "Multiplica 2*2 " . 2*2;
-echo "Divide 2/2 " . 2/2;
-echo "Módulo 2%2 " . 2%2;
-echo "Exponencial 2**2 " . 2**2;
-
-//Comparacion
-//Igual == valor    '9'==9
-//Igual === valor-tipo  9===9
-//Diferencias !=, valor
-//Diferencias !==, valor-tipo
-//<,>,<=,>=
-
-//Variables
-$app = 'name';
-$name='platzi';
-
-echo $$app; //platzi
-echo $app; //name
-echo $name; //platzi
+echo Text\Format::upperText('hola');
