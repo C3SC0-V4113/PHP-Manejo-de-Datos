fd1df3b1bff1923b774548548883370cedd6d846
Francisco Valle <vallecesco@gmail.com>
2021-01-06 22:05:58 -0600

    Analizando los distintos bucles y decisiones en php

---
+++ bloques.php
@@ -0,0 +1,35 @@
+<?php
+
+if ($condition) {
+    # code...
+} elseif ($condition) {
+    # code...
+} else {
+    # code...
+}
+
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+foreach ($variable as $key => $value) {
+    # code...
+}
+
+while ($a <= 10) {
+    # code...
+}
+
+do {
+    # code...
+} while ($a <= 10);
+
+for ($i=0; $i < 10; $i++) { 
+    # code...
+}
