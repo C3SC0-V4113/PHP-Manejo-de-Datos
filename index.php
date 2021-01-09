03fca4597d756a8c0e7ed65fb87223c560611090
Francisco Valle <vallecesco@gmail.com>
2021-01-09 15:15:25 -0600

    Entendiendo helpers y Format

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ require __DIR__ . '/vendor/autoload.php';
 echo upper('hola mundo cesque');
 
 echo upper(" Probando con comillas dobles");
+
+echo lower(" PROBANDO LOWERCASE");
--- src/Format.php
+++ src/Format.php
@@ -3,7 +3,12 @@
 namespace Text;
 
 class Format{
+
     public static function upperText($value){
         return strtoupper($value);
+    }
+
+    public static function lowerText($value){
+        return strtolower($value);
     }
 }
